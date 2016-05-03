e9096d2addb5f40bf96360932e5f493f1d812aff
Todd Michael Bushnell <todd@strataconsulting.com>
2016-05-02 18:56:01 -0700

    bump version to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Builds a DevOps workstation'
 long_description 'Builds a DevOps workstation'
 source_url 'https://github.com/strataconsulting/devopsbox-cook' if respond_to?(:source_url)
 issues_url 'https://github.com/strataconsulting/devopsbox-cook/issues' if respond_to?(:issues_url)
-version '0.1.0'
+version '0.1.1'
 
 depends 'chef-dk'
 depends 'build-essential'
