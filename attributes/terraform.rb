f78daee8460836bf5a6b888fe42805bdcfc7974c
Todd Michael Bushnell <todd@strataconsulting.com>
2016-08-26 16:23:26 -0700

    bump terraform to version 0.7.1

--- attributes/terraform.rb
+++ attributes/terraform.rb
@@ -1,5 +1,5 @@
 include_attribute 'terraform'
 
-default['terraform']['version'] = '0.7.0'
+default['terraform']['version'] = '0.7.1'
 default['terraform']['zipfile'] = "terraform_#{node['terraform']['version']}_" \
   "#{node['os']}_#{node['terraform']['arch']}.zip"
